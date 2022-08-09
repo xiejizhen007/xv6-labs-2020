a10127d9a091a104bd1209ae94328cc5634c1d84
xiejizhen007 <xiejizhen007@gmail.com>
2022-08-09 11:27:12 +0800

    kalloctest: cpu's freelist

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,16 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  int freepg_count;     // 记录当前 cpu 空闲页的数量
+} kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int id = 0; id < NCPU; id++) {
+    initlock(&kmem[id].lock, "kmem");
+    kmem[id].freepg_count = 0;
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -37,6 +41,9 @@ freerange(void *pa_start, void *pa_end)
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
+  // push_off();
+  // printf("cpu[%d]'s free page size: %d\n", cpuid(), kmem[cpuid()].freepg_count);
+  // pop_off();
 }
 
 // Free the page of physical memory pointed at by v,
@@ -56,10 +63,16 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  // turn interrupts off, get a current core number
+  push_off();
+  int id = cpuid();
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freepg_count++;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+  // turn interrupts on
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -70,11 +83,59 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  int id = cpuid();
+  acquire(&kmem[id].lock);
+  
+  r = kmem[id].freelist;
+  // 当前 cpu's freelist 仍有空闲页
+  if (kmem[id].freepg_count > 0) {
+    r = kmem[id].freelist;
+    kmem[id].freelist = r->next;
+    kmem[id].freepg_count--;
+  } else {
+    // 没空闲页了，去其他 cpu's freelist 偷
+    // printf("cpu[%d] no free page\n", id);
+    // 存在空闲页的 cpuid
+    int find_cpu = -1;
+    // 遍历所有 cpu，从中寻找存在空闲页的 cpuid
+    for (int i = 0; i < NCPU; i++) {
+      if (i == id) {
+        // 当前肯定是没有的了
+        continue;
+      }
+
+      acquire(&kmem[i].lock);
+      // 存在空闲页？
+      if (kmem[i].freepg_count > 0) {
+        find_cpu = i;
+      }
+      release(&kmem[i].lock);
+
+      if (find_cpu != -1) {
+        // 找到了
+        break;
+      }
+    }
+
+    // 其他 cpu 中存在空闲页，去他那偷
+    if (find_cpu != -1) {
+      // new id
+      int nid = find_cpu;
+      // printf("find a free page in cpu[%d] freelist\n", nid);
+      acquire(&kmem[nid].lock);
+      r = kmem[nid].freelist;
+      kmem[nid].freepg_count--;
+      kmem[nid].freelist = r->next;
+      release(&kmem[nid].lock);
+    } else {
+      // 实在是没有空闲的页了
+      r = 0;
+    }
+  }
+  
+  release(&kmem[id].lock);
+  pop_off();
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
